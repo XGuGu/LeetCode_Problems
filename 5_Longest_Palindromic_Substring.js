5cfe9baf711288e444fe79daa212724a84fcc409
lingjie zhang <zhanglingjie16303@gmail.com>
2019-02-05 15:18:51 -0500

    finish 5

--- 5_Longest_Palindromic_Substring.js
+++ 5_Longest_Palindromic_Substring.js
@@ -11,10 +11,47 @@
 // Output: "bb"
 
 var longestPalindrome = function(s) {
-  var max = '';
-  for (var i = 0; i < s.length; i++) {
+  if (s.length === 0) return '';
+  if (s.length === 1) return s;
+  if (s.length === 2) {
+    if (s[0] === s[1]) return s;
+    else return s[0];
+  }
 
-
+  const arr = s.split('');
+  // 存储回文数组
+  let r = [];
+  // 存储最大长度
+  let max = 0;
+  const judgeArr = (i, j, arr, isMid=false) => {
+    let k = 0;
+    while (i - k >= 0 && j + k < arr.length && arr[i - k] === arr[j + k]) k++;
+    if (isMid) {
+      if (2 * k + 1 > max) {
+        max = 2 * k + 1;
+        r = arr.slice(i - k + 1, j + k);
+      }
+    } else {
+      if (2 * k > max) {
+        max = 2 * k;
+        r = arr.slice(i - k + 1, j + k)
+      }
+    }
 
   }
-}
+  const getLongest = (mid, arr) => {
+    judgeArr(mid - 1, mid, arr);
+    judgeArr(mid, mid + 1, arr);
+    judgeArr(mid - 1, mid + 1, arr, true);
+  }
+
+  for (let i = 1; i < arr.length - 1; i++) {
+    getLongest(i, arr);
+  }
+
+  return r.join('');
+};
+
+对数组的回文判断, 假设i - 1, i, i + 1三个字符, 可能出现三种情况: i - 1 === i, i === i + 1, i - 1 === i + 1(i为索引)
+需要循环数组, 判断以上三种情况, 得到最长的回文字符.
+这类判断需要三个字符以上, 所以对长度0,1,2进行额外处理.
