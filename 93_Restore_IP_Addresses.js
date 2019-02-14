18b37c91d6a95062147763fcab014fc7c09e66da
lingjie zhang <zhanglingjie16303@gmail.com>
2019-02-14 18:58:35 -0500

    93

--- 93_Restore_IP_Addresses.js
+++ 93_Restore_IP_Addresses.js
@@ -8,3 +8,38 @@ Input: "25525511135"
 
 Backtracking 很少考
 记得写
+
+function restoreIpAddresses(s) {
+  const res = [];
+  dfs([], 0);
+  return res;
+
+  function dfs(prefix, idx) {
+    if (prefix.length === 4 && idx === s.length) {
+      res.push(prefix.join('.'));
+      return;
+    }
+
+    if (prefix.length === 4 || idx === s.length) {
+      return;
+    }
+
+    for (let r = idx; r < s.length; r++) {
+      if (r !== idx && s[idx] === '0') return;
+
+      const num = parseInt(s.slice(idx, r+1));
+      if (num > 255) {
+        return;
+      }
+      prefix.push(num);
+      dfs(prefix, r+1);
+      prefix.pop();
+    }
+  }
+}
+
+function dfs(prefix, idx) {
+  if (prefix.length === 4 && idx === s.length) {
+
+  }
+}
