388221b9318a6cac63e343bf619feff47c92ac3e
lingjie zhang <zhanglingjie16303@gmail.com>
2019-04-01 16:49:03 -0400

    6

---
+++ BB/6 ZigZag Conversion.js
@@ -0,0 +1,42 @@
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+And then read line by line: "PAHNAPLSIIGYIR"
+
+Write the code that will take a string and make this conversion given a number of rows:
+
+string convert(string s, int numRows);
+Example 1:
+
+Input: s = "PAYPALISHIRING", numRows = 3
+Output: "PAHNAPLSIIGYIR"
+Example 2:
+
+Input: s = "PAYPALISHIRING", numRows = 4
+Output: "PINALSIGYAHRPI"
+Explanation:
+
+P     I    N
+A   L S  I G
+Y A   H R
+P     I
+
+
+function convert(s, numRows) {
+  if (numRows === 1) {
+    return s;
+  }
+
+  const N = s.length;
+  const arr = [...Array(numRows)].map(r => []);
+
+  for (let i = 0; i < N; i++) {
+    const pos = i % (2*numRows-2);
+    const ii = pos < numRows ? pos : 2*numRows-2-pos;
+    arr[ii].push(s[i]);
+  }
+
+  return arr.map(r => r.join('')).join('');
+}
